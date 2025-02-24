7582b8c6fa0ee28626959769a330a2148fc8f5d6
Guido <46787485+guidoFortunato@users.noreply.github.com>
2025-02-24 17:26:51 -0300

    navigation app

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { Redirect } from "expo-router";
+import React from "react";
+
+const App = () => {
+
+  return <Redirect href="/home" />
+
+
+  // return (
+  //   <SafeAreaView>
+  //     <View className="mt-6 mx-2.5">
+  //       <Text
+  //         className="text-5xl text-blue-600"
+  //         style={{ fontFamily: "WorkSans-Medium" }}
+  //       >
+  //         Hola mundo
+  //       </Text>
+
+  //       <Text className="text-4xl text-primary font-work-black">
+  //         Hola mundo
+  //       </Text>
+  //       <Text className="text-3xl text-secondary-200">Hola mundo</Text>
+  //       <Text className="text-2xl text-tertiary">Hola mundo</Text>
+
+  //       <Link href="/products/index">Productos</Link>
+  //     </View>
+  //   </SafeAreaView>
+  // );
+};
+
+export default App;
---
+++ app/products/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+
+const ProductsScreen = () => {
+  return (
+    <View>
+      <Text>ProductsScreen</Text>
+    </View>
+  )
+}
+export default ProductsScreen
